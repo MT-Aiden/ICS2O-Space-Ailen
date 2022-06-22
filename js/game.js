590b325355bf4796f9fde9162fb472e75dcc604e
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-06-22 15:25:28 -0400

    game.js edit

--- js/game.js
+++ js/game.js
@@ -6,12 +6,25 @@
 // Created on: April 2022
 // This is the Phaser3 configuration file
 
+//* Game scene*/
 const config = {
-    type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
-    backgroundColor: 0x5f6e7a
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    }
+  },
+  //set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    //we place it in the middle of the page
+    autoCenter: Phaser.Scale.CENTER_BOTH
   }
-  
-  const game = new Phaser.Game(config)
-  console.log(game)
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
